b256b5d09c50ffa0f4dcc0435b48f21f259beaae
Kyle Brockman <kyle@brockma9.uits.uwm.edu>
2017-04-03 15:45:19 -0500

    Just opened with the Atom editor

--- macOS10.12Upgrade.sh
+++ macOS10.12Upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # Copyright (c) 2017 Jamf.  All rights reserved.
 #
@@ -12,7 +12,7 @@
 #                 notice, this list of conditions and the following disclaimer in the
 #                 documentation and/or other materials provided with the distribution.
 #               * Neither the name of the Jamf nor the names of its contributors may be
-#                 used to endorse or promote products derived from this software without 
+#                 used to endorse or promote products derived from this software without
 #                 specific prior written permission.
 #
 #       THIS SOFTWARE IS PROVIDED BY JAMF SOFTWARE, LLC "AS IS" AND ANY
@@ -26,14 +26,14 @@
 #       (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #       SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
 # This script was designed to be used in a Self Service policy to ensure specific
-# requirements have been met before proceeding with an inplace upgrade to macOS Sierra, 
-# as well as to address changes Apple has made to the ability to complete macOS upgrades 
-# silently. 
+# requirements have been met before proceeding with an inplace upgrade to macOS Sierra,
+# as well as to address changes Apple has made to the ability to complete macOS upgrades
+# silently.
 #
 # REQUIREMENTS:
 #			- Jamf Pro
@@ -47,12 +47,12 @@
 #
 # Created On: January 5th, 2017
 # Updated On: March 9th, 2017
-# 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # USER VARIABLES
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 ##Enter 0 for Full Screen, 1 for Utility window (screenshots available on GitHub)
 userDialog=0
@@ -65,16 +65,16 @@ heading="Please wait as we prepare your computer for macOS Sierra..."
 
 ##Title to be used for userDialog
 description="
-This process will take approximately 5-10 minutes. 
+This process will take approximately 5-10 minutes.
 Once completed your computer will reboot and begin the upgrade."
 
 ##Icon to be used for userDialog
 ##Default is macOS Sierra Installer logo which is included in the staged installer package
 icon=/Users/Shared/Install\ macOS\ Sierra.app/Contents/Resources/InstallAssistant.icns
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # SYSTEM CHECKS
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 ##Check if device is on battery or ac power
 pwrAdapter=$( /usr/bin/pmset -g ps )
@@ -102,9 +102,9 @@ else
 	/bin/echo "Disk Check: ERROR - ${freeSpace%.*}GB Free Space Detected"
 fi
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # CREATE FIRST BOOT SCRIPT
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 /bin/mkdir /usr/local/jamfps
 
@@ -125,32 +125,32 @@ exit 0" > /usr/local/jamfps/finishOSInstall.sh
 /usr/sbin/chown root:admin /usr/local/jamfps/finishOSInstall.sh
 /bin/chmod 755 /usr/local/jamfps/finishOSInstall.sh
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # LAUNCH DAEMON
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-/bin/echo "<?xml version="1.0" encoding="UTF-8"?> 
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"> 
-<plist version="1.0"> 
+/bin/echo "<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
 <dict>
-	<key>Label</key> 
-	<string>com.jamfps.cleanupOSInstall</string> 
-	<key>ProgramArguments</key> 
-	<array> 
+	<key>Label</key>
+	<string>com.jamfps.cleanupOSInstall</string>
+	<key>ProgramArguments</key>
+	<array>
 		<string>/usr/local/jamfps/finishOSInstall.sh</string>
 	</array>
 	<key>RunAtLoad</key>
 	<true/>
-</dict> 
+</dict>
 </plist>" > /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
 
 ##Set the permission on the file just made.
 /usr/sbin/chown root:wheel /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
 /bin/chmod 644 /Library/LaunchDaemons/com.jamfps.cleanupOSInstall.plist
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # APPLICATION
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if [[ ${pwrStatus} == "OK" ]] && [[ ${spaceStatus} == "OK" ]]; then
     ##Launch jamfHelper
@@ -171,8 +171,8 @@ if [[ ${pwrStatus} == "OK" ]] && [[ ${spaceStatus} == "OK" ]]; then
     /bin/sleep 3
 else
 	/bin/echo "Launching jamfHelper Dialog (Requirements Not Met)..."
-	/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "$title" -icon "$icon" -heading "Requirements Not Met" -description "We were unable to prepare your computer for macOS Sierra. Please ensure you are connected to power and that you have at least 15GB of Free Space. 
-	
+	/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType utility -title "$title" -icon "$icon" -heading "Requirements Not Met" -description "We were unable to prepare your computer for macOS Sierra. Please ensure you are connected to power and that you have at least 15GB of Free Space.
+
 	If you continue to experience this issue, please contact the IT Support Center." -iconSize 100 -button1 "OK" -defaultButton 1
 fi
 
